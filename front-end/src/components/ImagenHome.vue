beaef9dff4ea19ed9819eb4dfebac86f3b7a41e5
Laura Rodriguez King <90001118+lauriy91@users.noreply.github.com>
2021-10-11 19:28:16 -0500

    update components

--- front-end/src/components/ImagenHome.vue
+++ front-end/src/components/ImagenHome.vue
@@ -2,11 +2,6 @@
 
   <div class= "contenedor_secundario">
 
-    <!--Contenedor botones de la parte superior derecha-->
-    <div class="buscador_idioma">
-      <busqueda-home />
-    </div>
-
     <!-- CONTENEDOR IMAGEN DE FONDO -->
     <div class="contFondo">
       <img src="https://i.ibb.co/r4YGF3V/img-intro.png" />
@@ -17,27 +12,15 @@
 </template>
 
 <script>
-import BusquedaHome from "../components/BusquedaHome.vue";
 export default {
-  components: {
-    BusquedaHome,
-  },
-};
+}
 </script>
 
 <style>
-
 .contenedor_secundario{
   display: flex;
 }
-
-img{
-  margin: -4% 0% 3% 14%;
-  width: 130%;
-}
-
 .buscador_idioma{
-  padding-top: 5%;
+  margin-top: -25%;
 }
-
 </style>
