603ed5eb07580e3aa9b36ae17a95a416d291249b
Laura Rodriguez King <90001118+lauriy91@users.noreply.github.com>
2021-10-11 19:25:15 -0500

    merge

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -5,19 +5,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    personajes: []
+    alojamientos: [],
+    reservas: []
   },
   mutations: {
-    setPersonajes(state, payload){
-      state.personajes = payload;
+    setAlojamientos(state, payload){
+      state.alojamientos = payload;
+    },
+    pushAlojamiento(state, obj){
+      state.alojamientos.push(obj);
+    },
+    setReservas(state, payload){
+      state.reservas = payload;
     }
   },
   actions: {
-    async getPersonajes({commit}){
-      const peticion = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
+    async cargarAlojamientos({commit}){
+      //GET
+      const peticion = await fetch('https://aiohosting.herokuapp.com/DescripcionHostal');
       const data = await peticion.json();
       console.log(data);
-      commit('setPersonajes', data);
+      commit('setAlojamientos', data);
+    },
+    async registrarAlojamiento({commit}, objAlojamientos){
+      //POST
+      const peticion = await fetch('https://aiohosting.herokuapp.com/DescripcionHostal', {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(objAlojamientos)
+      });
+      const data = await peticion.json();
+      commit('pushAlojamiento', data);
+      console.log(data);
+    },
+    async cargarReservas({commit}){
+      const peticion = await fetch('https://aiohosting.herokuapp.com/Formulario');
+      const data = await peticion.json();
+      console.log(data);
+      commit('setReservas', data);
+    },
+    async eliminarAlojamiento({commit}, obj){
+      const peticion = await fetch('https://aiohosting.herokuapp.com/DescripcionHostal', {
+        method: 'DELETE',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+      });
     }
   },
   modules: {
